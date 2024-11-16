433fedc296ef7189c7f5109faceda95ed1dae278
AlfazaNaufalZakiy <118534903+AlfazaNaufalZakiy@users.noreply.github.com>
2024-11-16 10:04:29 +0700

    connect firebase

--- register.js
+++ register.js
@@ -1,5 +1,14 @@
 // Firebase configuration
-import firebaseConfig from './config.js';
+const firebaseConfig = {
+  apiKey: "AIzaSyBXIAMWLGxD7oMf05KCPArmfqaV5CnieAE",
+  authDomain: "funenglishlab-4bf5c.firebaseapp.com",
+  projectId: "funenglishlab-4bf5c",
+  storageBucket: "funenglishlab-4bf5c.firebasestorage.app",
+  messagingSenderId: "94441181912",
+  appId: "1:94441181912:web:6e9f5e337175adb3263dad",
+  measurementId: "G-2X2QTL0XE1"
+};
+
 firebase.initializeApp(firebaseConfig);
 
   
