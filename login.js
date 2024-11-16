ebbc3d406dac0af3680726e5b8c41ee4192b2788
AlfazaNaufalZakiy <118534903+AlfazaNaufalZakiy@users.noreply.github.com>
2024-11-16 09:30:23 +0700

    amankan API

--- login.js
+++ login.js
@@ -1,16 +1,9 @@
 // Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBXIAMWLGxD7oMf05KCPArmfqaV5CnieAE",
-    authDomain: "funenglishlab-4bf5c.firebaseapp.com",
-    projectId: "funenglishlab-4bf5c",
-    storageBucket: "funenglishlab-4bf5c.firebasestorage.app",
-    messagingSenderId: "94441181912",
-    appId: "1:94441181912:web:6e9f5e337175adb3263dad",
-    measurementId: "G-2X2QTL0XE1"
-  };
+import firebaseConfig from './config.js';
+firebase.initializeApp(firebaseConfig);
+
   
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+
   
   function loginUser(event) {
     event.preventDefault();
