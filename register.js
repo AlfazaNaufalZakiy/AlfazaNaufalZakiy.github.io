2853bfadf169d23e9cbad0f48b342f431fe564fb
AlfazaNaufalZakiy <118534903+AlfazaNaufalZakiy@users.noreply.github.com>
2024-11-18 08:00:03 +0700

    fix API

--- register.js
+++ register.js
@@ -1,12 +1,12 @@
 // Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBXIAMWLGxD7oMf05KCPArmfqaV5CnieAE",
-  authDomain: "funenglishlab-4bf5c.firebaseapp.com",
-  projectId: "funenglishlab-4bf5c",
-  storageBucket: "funenglishlab-4bf5c.firebasestorage.app",
-  messagingSenderId: "94441181912",
-  appId: "1:94441181912:web:6e9f5e337175adb3263dad",
-  measurementId: "G-2X2QTL0XE1"
+var firebaseConfig = {
+  apiKey: firebaseConfig.apiKey,
+  authDomain: firebaseConfig.authDomain,
+  projectId: firebaseConfig.projectId,
+  storageBucket: firebaseConfig.storageBucket,
+  messagingSenderId: firebaseConfig.messagingSenderId,
+  appId: firebaseConfig.appId,
+  measurementId: firebaseConfig.measurementId
 };
 
 firebase.initializeApp(firebaseConfig);
