177e2f1cd02a08fb35d1eb9195f106ce65626eca
AlfazaNaufalZakiy <118534903+AlfazaNaufalZakiy@users.noreply.github.com>
2024-11-18 10:26:48 +0700

    coba logout

--- auth.js
+++ auth.js
@@ -7,4 +7,16 @@ function requireAuth() {
   }
   
   requireAuth();
-  
+
+
+function logoutUser() {
+    firebase.auth().signOut()
+        .then(() => {
+            alert("You have successfully logged out!");
+            window.location.href = "index.html"; // Redirect ke halaman login
+        })
+        .catch((error) => {
+            console.error("Error during logout:", error);
+            alert("Failed to log out. Please try again.");
+        });
+}
